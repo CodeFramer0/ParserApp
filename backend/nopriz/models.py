d059b78e5ef721d764cee8e98a867f2c93d29d48
CodeFramer <52332286+CodeFramer0@users.noreply.github.com>
2024-09-26 00:11:59 +0500

    refactoring nopriz.models

--- backend/nopriz/models.py
+++ backend/nopriz/models.py
@@ -7,7 +7,7 @@ class NoprizFiz(models.Model):
         ("EXCLUDED", "Исключен"),
     ]
 
-    id_number_img = models.URLField(max_length=200, unique=True)
+    id_number_img = models.URLField(max_length=200)
     full_name_img = models.URLField(max_length=200)
     date_of_inclusion_protocol_img = models.URLField(
         max_length=200, null=True, blank=True
@@ -16,19 +16,19 @@ class NoprizFiz(models.Model):
     id_number = models.CharField(max_length=50)
     full_name = models.CharField(max_length=255)
     date_of_inclusion_protocol = models.CharField(max_length=50, null=True, blank=True)
-    date_of_modification = models.CharField(max_length=50, blank=True, null=True)
+    date_of_modification = models.CharField(max_length=50, null=True, blank=True)
     date_of_issue_certificate = models.CharField(max_length=50, null=True, blank=True)
-    type_of_work = models.TextField(max_length=120, null=True, blank=True)
+    type_of_work = models.TextField(null=True, blank=True)
     date_of_exclusion = models.CharField(max_length=50, null=True, blank=True)
     status_worker = models.CharField(
-        max_length=100, choices=STATUS_CHOICES, blank=True, null=True
+        max_length=100, choices=STATUS_CHOICES, null=True, blank=True
     )
     verified_id_number = models.BooleanField(default=False)
     id_number_verification_attempts = models.PositiveSmallIntegerField(default=0)
     is_parsed = models.BooleanField(default=False)
 
     def __str__(self):
-        return f" {self.id_number} {self.full_name}"
+        return f"{self.id_number} {self.full_name}"
 
     class Meta:
         verbose_name = "Физ-лицо"
@@ -45,18 +45,18 @@ class NoprizYr(models.Model):
         ("EXCLUDED", "Исключен"),
     ]
 
-    id_number = models.CharField(unique=False)
-    name_cpo = models.CharField()
-    status = models.CharField(choices=STATUS_CHOICES)
-    name_of_the_member_cpo = models.CharField()
-    inn = models.CharField()
-    ogrn = models.CharField()
-    date_of_termination = models.CharField(null=True, blank=True)
-    date_of_registration = models.CharField(null=True, blank=True)
-    director = models.TextField(max_length=120)
+    id_number = models.CharField(max_length=50)
+    name_cpo = models.CharField(max_length=255)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES)
+    name_of_the_member_cpo = models.TextField()
+    inn = models.CharField(max_length=50)
+    ogrn = models.CharField(max_length=50)
+    date_of_termination = models.CharField(max_length=50, null=True, blank=True)
+    date_of_registration = models.CharField(max_length=50, null=True, blank=True)
+    director = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return f" {self.id_number}"
+        return f"{self.id_number}"
 
     class Meta:
         verbose_name = "Юр-лицо"
